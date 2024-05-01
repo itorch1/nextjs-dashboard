75629695c7cb31d638ae2a9b13ab7976f877420d
Cinderhelm <itorch100@gmail.com>
2024-05-01 17:32:33 +0300

    SSG Test last

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,7 +14,7 @@ import { unstable_noStore as noStore } from 'next/cache';
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  // noStore();
+  noStore();
 
   try {
     // Artificially delay a response for demo purposes.
@@ -35,7 +35,7 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
-  noStore();
+  // noStore();
 
   try {
     const data = await sql<LatestInvoiceRaw>`
@@ -57,7 +57,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
-  noStore();
+  // noStore();
 
   try {
     // You can probably combine these into a single SQL query
